19bfb7711b205780ed57616c12c2923aa5c1342c
= <=>
2020-12-21 19:47:09 -0600

    Herencia en python

--- Python/account.py
+++ Python/account.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class Account:
      id = int
      name = str
@@ -10,11 +10,3 @@ class Account:
           self.name = name
           self.document = document
           
-=======
-class Account:
-     id = int
-     name = str
-     document = str
-     emai = str
-     password =str
->>>>>>> 59bbb29f13d3aeba7930e5172789833335f4f995
