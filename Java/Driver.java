9b3e3390a039cdf5ac5c7e246a13776e70f7add9
= <=>
2020-12-21 21:50:09 -0600

    Herencia account en java

---
+++ Java/Driver.java
@@ -0,0 +1,11 @@
+public class Driver extends Account {
+    String email;
+    String password;
+
+    public Driver(String name, String document, String email,String password) {
+        super(name,document);
+        this.email = email;
+        this.password = password;
+
+    }
+}
