99d825d1eef0f11d249cc4ef424d93b72eb933a3
= <=>
2020-12-21 21:30:39 -0600

    Herencia en java Scrip Creo :C

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,8 @@
 var car = new Car("AW456", new Account("Isaac Hdz","ADF342"));
 car.passenger = 4;
 car.printDataCar();
+
+
+var UberX = new UberX("AXC143", new Account("Alma Veronica","ASW234"), "GYP","Spark");
+UberX.passenger = 4;
+UberX.printDataCar();
