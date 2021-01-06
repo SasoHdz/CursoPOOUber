264080e16e0f82d28fc855ca45d895682aff75e4
= <=>
2021-01-05 21:47:15 -0600

    Creando constructor de Doctor

--- Java/JavaPOO/Doctor.java
+++ Java/JavaPOO/Doctor.java
@@ -4,8 +4,15 @@ public class Doctor {
     String name;
     String speciality;
 
+    public Doctor(int id, String name, String speciality) {
+        this.id = id;
+        this.name = name;
+        this.speciality = speciality;
+    }
+
     //COMPORTAMIENTOS   
     public void showName(){
         System.out.println(name);
     }
+
 }
--- Java/JavaPOO/Main.java
+++ Java/JavaPOO/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         
-        Doctor doctor = new Doctor();
-        doctor.name = "Juan de Dios";
-        doctor.speciality = "Pediatra";
-
+        Doctor doctor = new Doctor(01,"Juan de Dios","Pediatra");
         System.out.println("Nombre del doctor: "+doctor.name+" especialidad: "+doctor.speciality);
         
     }
