dce9235741ee3528db4e5909efd59577ab51c230
= <=>
2021-01-05 21:45:00 -0600

    Iniciando con POO JAVA, primera clase

---
+++ Java/JavaPOO/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Doctor doctor = new Doctor();
+        doctor.name = "Juan de Dios";
+        doctor.speciality = "Pediatra";
+
+        System.out.println("Nombre del doctor: "+doctor.name+" especialidad: "+doctor.speciality);
+        
+    }
+}
