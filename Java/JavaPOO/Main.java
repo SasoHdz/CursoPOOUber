931b620fcc2c21b6e6e4f2f2460018f222b97307
= <=>
2021-01-06 22:42:47 -0600

    Modificando UI --> declarando constantes con Final

--- Java/JavaPOO/Main.java
+++ Java/JavaPOO/Main.java
@@ -3,16 +3,6 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
         
-        Doctor doctor = new Doctor();
-        doctor = new Doctor("Juan de Dios","Pediatra");
-        doctor.showName();
-        System.out.println(doctor.id);
-
-        Doctor myDoctor = new Doctor();
-        myDoctor = new Doctor("Alma Veronica","Cirujadana");
-        myDoctor.showName();
-        System.out.println(myDoctor.id);
-
         showMenu();
     }
 }
