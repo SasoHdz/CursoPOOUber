15aa58267894c6687de504b72663f5a4d23e489c
Isaac Hernandez Resendiz <isaacrdzcmd@gmail.com>
2021-01-08 16:01:25 -0600

    Arreglando el merge

--- Java/JavaPOO/Doctor.java
+++ Java/JavaPOO/Doctor.java
@@ -1,9 +1,9 @@
 public class Doctor {
 
     static int id;
-    String name;
-    String speciality;
-    String email;
+    private String name;
+    private String speciality;
+    private String email;
 
     public Doctor(String name, String speciality) {
         this.name = name;
@@ -11,9 +11,33 @@ public class Doctor {
         id++;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
     //COMPORTAMIENTOS   
     public void showName(){
         System.out.println(name);
     }
-
 }
